02366080184642deb450f3431a9d3cb50ed7d042
Will Osborne <will.osborne@morganstanley.com>
2024-03-28 15:44:08 +0000

    Update commands to find pattern on -p

--- cli/src/commands/visualize/visualize.ts
+++ cli/src/commands/visualize/visualize.ts
@@ -1,8 +1,8 @@
 import { visualize } from 'calm-visualizer';
 import * as fs from 'node:fs';
 
-export default async function({input, output}: {input: string, output: string}) {
-    const calm = fs.readFileSync(input, 'utf-8');
+export default async function({pattern, output}: {pattern: string, output: string}) {
+    const calm = fs.readFileSync(pattern, 'utf-8');
     const svg = await visualize(calm); 
     fs.writeFileSync(output, svg);
 }
--- cli/src/index.ts
+++ cli/src/index.ts
@@ -10,7 +10,7 @@ program
 
 program
     .command('visualize')
-    .requiredOption('-i, --input <file>', 'The full path to the CALM Specification.')
+    .requiredOption('-p, --pattern <file>', 'The full path to the CALM Specification.')
     .requiredOption('-o, --output <file>', 'The name of the file to output the SVG in.', 'calm-visualization.svg')
     .description('Produces an SVG file representing a visualization of the CALM Specification.')
     .action(visualize);
@@ -18,11 +18,11 @@ program
 program
     .command('generate')
     .description('Generate an instantiation from a CALM pattern file.')
-    .requiredOption('-s, --source <source>', 'Path to the pattern file to use. May be a file path or a URL.')
+    .requiredOption('-p, --pattern <source>', 'Path to the pattern file to use. May be a file path or a URL.')
     .requiredOption('-o, --output <output>', 'Path location at which to output the generated file.')
     .option('-v, --verbose', 'Enable verbose logging.')
     .action((options) => {
-        runGenerate(options.source, options.output, !!options.verbose);
+        runGenerate(options.pattern, options.output, !!options.verbose);
     });
 
 program.parse(process.argv);
